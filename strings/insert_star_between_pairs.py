90408c32d71324acc3ce348a84cc36d8e4307d16
MaxNRG <maxnrg@gmail.com>
2019-12-10 18:34:31 +0200

    insert_star_between_pairs

--- strings/insert_star_between_pairs.py
+++ strings/insert_star_between_pairs.py
@@ -12,15 +12,17 @@ abbba -> ab*b*ba
 bbb -> b*b*b
 """
 
+
 def insert_star_between_pairs(a_string):
     if type(a_string) != str:
         return None
     s = a_string[0]
     for i in range(1, len(a_string)):
-        if a_string[i] == a_string[i-1]:
+        if a_string[i] == a_string[i - 1]:
             s += ('*')
         s += (a_string[i])
     return s
+
 
 print(insert_star_between_pairs("cac"))
 print(insert_star_between_pairs("cc"))
